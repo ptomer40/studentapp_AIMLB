f111deed83f3f5876fcef009e0a3b65b3a288512
ptomer40 <ptomer40@yahoo.com>
2025-04-30 20:23:25 +0530

    admin added

--- reactloginreg/src/component/Home.jsx
+++ reactloginreg/src/component/Home.jsx
@@ -6,6 +6,11 @@ function Home() {
     <div style={{backgroundColor:'#965954',color:'white',fontSize:'25px'}}>Welcome to Student App</div>
     <div>
         <nav>
+          <div>
+          <ul>
+          <li><Link to='/studentadmin'>Admin</Link></li>
+          </ul>
+          </div>
             <ul>
        <li><Link to='/login'>Login</Link></li>
        <li><Link to='/register'>Registration</Link></li>
